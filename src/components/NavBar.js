458909786946895113671f85839ee897807bdeb7
luna bartolozzi <luna.bartolozzi@gmail.com>
2020-12-27 13:58:20 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <header href="#" className="navbar">
+        <a href="#" className="navbar-logo">
+          Store
+        </a>
+        <a href="#" href="#" className="navbar-item">
+          Inicio
+        </a>
+        <a href="#" className="navbar-item">
+          Tienda
+        </a>
+        <a href="#" className="navbar-item">
+          Sobre nosotros
+        </a>
+        <a href="#" className="navbar-item">
+          Contacto
+        </a>
+        <a href="#" className="navbar-item">
+          Info
+        </a>
+      </header>
+    </>
+  );
+}
+export default Navbar;
